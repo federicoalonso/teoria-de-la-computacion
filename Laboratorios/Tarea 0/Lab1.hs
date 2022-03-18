bc67261168d65638347f5b4ea2b1f4e442d61f08
Federico Alonso <f.nicolas.alonso@gmail.com>
2022-03-18 19:38:55 -0300

    lab1

---
+++ Laboratorios/Tarea 0/Lab1.hs
@@ -0,0 +1,9 @@
+Type M := [(String, Integer)]
+
+
+
+-- El orden de la cuarta regla si importa, ejemplo: no es conmutativa
+-- primero e1 nos muestra, puede cambiar la memoria, y luego evalua en la segunda que tambien puede cambiar la memoria
+-- usar div y mod, no / y %
+-- el dos da error
+-- division entre cero?
