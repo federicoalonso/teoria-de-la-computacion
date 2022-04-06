e2950914e2a33aee1bc1a70ce3d5ffd8003054e7
Federico Alonso <f.nicolas.alonso@gmail.com>
2022-04-06 19:23:05 -0300

    Dia de laboratorio

--- Laboratorios/Tarea 1/Lab1.hs
+++ Laboratorios/Tarea 1/Lab1.hs
@@ -2,3 +2,24 @@ module Lab1 where
 
 -- Federico Alonso 182999
 
+type B = (String,([String],E))
+
+data E = X String
+    | C String
+    | Lam [String] E
+    | App E [E]
+    | Case E [B]
+    | Rec [(String,E)]
+
+type Sigma = [(String,E)]
+
+-- buscar :: String -> Sigma -> E
+-- buscar x [] = error "No se encuentra el valor."
+-- buscar x (m:ms)
+--     | ((fst m) == x) = snd m 
+--     | otherwise = buscar ms x
+
+sust :: E -> Sigma -> Maybe E
+sust (X x) sig = lookup x sig
+
+-- sust (X "x") [("x", C "0")]
